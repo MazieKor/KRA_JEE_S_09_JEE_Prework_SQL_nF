e6f4c7baa1be075d826becafbbe422ba85341343
MazieKor <maciej.coders@gmail.com>
2020-08-23 10:00:52 +0200

    Dodanie 5 uczniów.

---
+++ 03_Dodawanie_danych/03_Zadanie_3/zadanie3.sql
@@ -0,0 +1,5 @@
+INSERT INTO students VALUES (21, 'Marek', 'Markiewicz', 'marek.markiewicz@op.pl', 6);
+INSERT INTO students VALUES (22, 'Ala', 'Alewska', 'ala@op.pl', 6);
+INSERT INTO students VALUES (23, 'Iza', 'Irkowska', 'ira@op.pl', 6);
+INSERT INTO students VALUES (24, 'Wojciech', 'wojcieszek', 'wojtek.w@op.pl', 6);
+INSERT INTO students VALUES (25, 'Agata', 'Zuzańska', 'zuzka@op.pl', 6);
