45b68f21811ca0e032b7614bbd756c09de13f2ad
MazieKor <maciej.coders@gmail.com>
2020-08-23 10:35:26 +0200

    Skasowanie ocen o mark_id 101-110.

---
+++ 04_Modyfikacja_i_usuwanie_danych/05_Zadanie_5/zadanie5.sql
@@ -0,0 +1,6 @@
+DELETE FROM marks WHERE teacher_id = 1 AND mark_id BETWEEN 101 and 110;
+DELETE FROM marks WHERE teacher_id = 2 AND mark_id BETWEEN 101 and 110;
+DELETE FROM marks WHERE teacher_id = 3 AND mark_id BETWEEN 101 and 110;
+DELETE FROM marks WHERE teacher_id = 4 AND mark_id BETWEEN 101 and 110;
+DELETE FROM marks WHERE teacher_id = 5 AND mark_id BETWEEN 101 and 110;
+DELETE FROM marks WHERE teacher_id = 6 AND mark_id BETWEEN 101 and 110;
